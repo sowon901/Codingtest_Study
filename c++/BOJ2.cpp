1e2d9feee47c1730d0fd82e7a6797a4792e81db5
sowon901 <87898185+sowon901@users.noreply.github.com>
2023-10-17 19:26:36 +0900

    [BOJ] 약수/브론즈1

---
+++ c++/BOJ2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <vector>
+using namespace std;
+
+int main() {
+
+	int count, divisor;
+	cin >> count;
+	int answer = 0;
+
+	vector<int> array;
+
+	for (int i = 0; i < count; i++) {
+		cin >> divisor;
+		array.push_back(divisor);
+
+	}
+	sort(array.begin(), array.end());
+
+	if (count == 1)
+		answer = pow(array[0], 2);
+	else {
+		answer = array[0] * array[count - 1];
+	}
+
+	cout << answer;
+
+	return 0;
+}
